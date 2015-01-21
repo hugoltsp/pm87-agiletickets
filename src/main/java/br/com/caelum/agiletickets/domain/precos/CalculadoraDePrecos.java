b0025025ae0213b2e2cfd26a694416c71c69c398
BHRother <bh.rother@yahoo.com.br>
2015-01-21 21:14:03 -0200

    Movendo valores para o Enum e adaptando método

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -3,41 +3,32 @@ package br.com.caelum.agiletickets.domain.precos;
 import java.math.BigDecimal;
 
 import br.com.caelum.agiletickets.models.Sessao;
+import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
 public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
+		return calculaPrecoUltimoLote(sessao, quantidade);
+	}
+
+	private static BigDecimal calculaPrecoUltimoLote(Sessao sessao,
+			Integer quantidade) {
+
 		BigDecimal preco = sessao.getPreco();
+		TipoDeEspetaculo tipo = sessao.getEspetaculo().getTipo();
 
-		switch (sessao.getEspetaculo().getTipo()) {
-		case CINEMA:
-		case SHOW:
-			preco = calculaPrecoUltimoLote(sessao, preco, 0.05, 0.10);
-			break;
-		case BALLET:
-		case ORQUESTRA:
-			preco = calculaPrecoUltimoLote(sessao, preco, 0.50, 0.20);
+		if (sessao.getPorcentagemOcupacao() <= tipo.getTaxaOcupacao()) {
+			preco = preco.add(preco.multiply(BigDecimal.valueOf(tipo
+					.getTaxaUltimoLote())));
+		}
 
-			if (sessao.getDuracaoEmMinutos() > 60) {
-				preco = preco.add(sessao.getPreco().multiply(
-						BigDecimal.valueOf(0.10)));
-			}
-			break;
-		case TEATRO:
-			break;
+		if (sessao.getDuracaoEmMinutos() != null
+				&& sessao.getDuracaoEmMinutos() > 60) {
+			preco = preco.add(sessao.getPreco().multiply(
+					BigDecimal.valueOf(tipo.getTaxaTempo())));
 		}
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
-	}
-
-	private static BigDecimal calculaPrecoUltimoLote(Sessao sessao, BigDecimal preco,
-			double taxaOcupacao, double taxaUltimoLote) {
-
-		if (sessao.getPorcentagemOcupacao() <= taxaOcupacao) {
-			preco = preco
-					.add(preco.multiply(BigDecimal.valueOf(taxaUltimoLote)));
-		}
-		return preco;
 	}
 
 }
--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -1,7 +1,38 @@
 package br.com.caelum.agiletickets.models;
 
 public enum TipoDeEspetaculo {
-	
-	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
-	
+
+	CINEMA(0.05, 0.10), SHOW(0.05, 0.10), TEATRO, BALLET(0.50, 0.20, 0.10), ORQUESTRA(
+			0.50, 0.20, 0.10);
+
+	private double taxaOcupacao;
+	private double taxaUltimoLote;
+	private double taxaTempo;
+
+	TipoDeEspetaculo() {
+		this(0, 0, 0);
+	}
+
+	TipoDeEspetaculo(double taxaOcupacao, double taxaUltimoLote) {
+		this(taxaOcupacao, taxaUltimoLote, 0);
+	}
+
+	TipoDeEspetaculo(double taxaOcupacao, double taxaUltimoLote,
+			double taxaTempo) {
+		this.taxaOcupacao = taxaOcupacao;
+		this.taxaUltimoLote = taxaUltimoLote;
+		this.taxaTempo = taxaTempo;
+	}
+
+	public double getTaxaOcupacao() {
+		return taxaOcupacao;
+	}
+
+	public double getTaxaUltimoLote() {
+		return taxaUltimoLote;
+	}
+
+	public double getTaxaTempo() {
+		return taxaTempo;
+	}
 }
