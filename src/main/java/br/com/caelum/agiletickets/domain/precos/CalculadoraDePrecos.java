4541c4b94f30f684a53980c766d87b5fcb204484
BHRother <bh.rother@yahoo.com.br>
2015-01-21 19:59:13 -0200

    Extraindo metodo para remover duplicacao

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -14,36 +14,11 @@ public class CalculadoraDePrecos {
 		case CINEMA:
 		case SHOW:
 			// quando estiver acabando os ingressos...
-			if ((sessao.getTotalIngressos() - sessao.getIngressosReservados())
-					/ sessao.getTotalIngressos().doubleValue() <= 0.05) {
-				preco = sessao.getPreco().add(
-						sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			} else {
-				preco = sessao.getPreco();
-			}
+			preco = calculaPreco(sessao, 0.05, 0.10);
 			break;
 		case BALLET:
-			if ((sessao.getTotalIngressos() - sessao.getIngressosReservados())
-					/ sessao.getTotalIngressos().doubleValue() <= 0.50) {
-				preco = sessao.getPreco().add(
-						sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
-
-			if (sessao.getDuracaoEmMinutos() > 60) {
-				preco = preco.add(sessao.getPreco().multiply(
-						BigDecimal.valueOf(0.10)));
-			}
-			break;
 		case ORQUESTRA:
-			if ((sessao.getTotalIngressos() - sessao.getIngressosReservados())
-					/ sessao.getTotalIngressos().doubleValue() <= 0.50) {
-				preco = sessao.getPreco().add(
-						sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
+			preco = calculaPreco(sessao, 0.50, 0.20);
 
 			if (sessao.getDuracaoEmMinutos() > 60) {
 				preco = preco.add(sessao.getPreco().multiply(
@@ -57,6 +32,18 @@ public class CalculadoraDePrecos {
 		}
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
+	}
+
+	private static BigDecimal calculaPreco(Sessao sessao, double taxa1, double taxa2) {
+		BigDecimal preco;
+		if ((sessao.getTotalIngressos() - sessao.getIngressosReservados())
+				/ sessao.getTotalIngressos().doubleValue() <= taxa1) {
+			preco = sessao.getPreco().add(
+					sessao.getPreco().multiply(BigDecimal.valueOf(taxa2)));
+		} else {
+			preco = sessao.getPreco();
+		}
+		return preco;
 	}
 
 }
